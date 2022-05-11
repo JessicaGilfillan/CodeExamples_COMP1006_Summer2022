0bff34909ef7c1340757e82794d0383cd99a7a26
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-05-10 20:57:35 -0400

    week one - PHP basics

---
+++ week_one/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMP1006 - Week One</title>
+</head>
+
+<body>
+
+    <?php
+
+    //1.  Running Your First PHP Script 
+
+
+    //2. Variable Intialization and Assignment & Displaying in the Browser 
+
+
+    /* Practice Activity One - create a variable to store your first name, create a variable to store your last name, combine first and last and echo on the screen */
+
+
+    /* 3.  Data Types & Functions */
+
+    //array built in function, three types of arrays 
+
+    //indexed arrays 
+
+    //associative arrays 
+
+    //multidimensional arrays 
+
+
+
+    /* Practice Activity 
+    a.) Access the second value in the indexed array and store in a variable 
+    b.) Access the third value in the associative array and store in a variable 
+    c.) Access the third item of the array inside of the array and store in a variable 
+    */
+
+
+    //Practice Challenge - loop through the items in the first array and print to the screen 
+
+
+    //user defined functions 
+
+    //function definition
+
+    //function invocation 
+
+
+    //Practice Activity - create a simple function to accept a message as an argument, call that function 
+
+    // 4. Objects in PHP 
+    //To create an Object in PHP, use the new operator to instantiate a class
+
+    //instantiate a new object based on the class 
+
+
+    /*Practice Activity - Create a class to represent a person, with properties for first and last name and a method to display first and last name in the browser. Instantiate a new object to represent you based on this class, call the method */
+
+    //5. Scope in PHP 
+
+    ?>
+</body>
+
+</html>
