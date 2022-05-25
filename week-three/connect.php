e6e2f66beab9aea231fbca0d01b72d4c10bda4af
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-05-25 12:34:28 -0400

    update connect.php

--- week-three/connect.php
+++ week-three/connect.php
@@ -1,12 +1,3 @@
 <?php
-try {
-    $dsn = 'mysql:host=172.31.22.43;dbname=Jessica001';
-    $username = 'Jessica001'; 
-    $password = 'KXeorM36ld';
-    $db = new PDO($dsn, $username, $password);
-}
-catch(PDOException $e) {
-   $error_message = $e->getMessage(); 
-   echo "<p> Whoops! Our bad! Something happened while trying to connect. It was this -  $error_message </p>"; 
-}
+
 ?>
