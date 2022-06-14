03f23ec8fffba02d6f5ff2d0e414709849cbd90d
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-06-14 12:51:02 -0400

    Add files via upload

---
+++ labs/lab_solutions/lab_two/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `students` (
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `age` int(10) NOT NULL,
+  `location` varchar(100) NOT NULL
+);
---
+++ labs/lab_solutions/lab_two/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lab Two - Form Validation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous" async></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Lab Two - Form Validation </h1>
+        <!--add client-side form validation using HTML5 validation feature-->
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+            <div class="form-group">
+                <label for="name"> Name: </label>
+                <input type="text" name="name" id="firstname" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email"> Email: </label>
+                <input type="text" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="age"> Age: </label>
+                <input type="text" name="age" id="age" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="location"> Location: </label>
+                <input type="text" name="location" id="location" class="form-control">
+            </div>
+                <input type="submit" value="Add Student" class="btn btn-primary" name="submit">
+
+        </form>
+    </div>
+    <?php
+    if (isset($_POST['submit'])) {
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
+        $age = filter_input(INPUT_POST, 'age');
+        $location = filter_input(INPUT_POST, 'location');
+
+        //add server-side form validation here!
+
+        //connect to db 
+        $dsn = 'mysql:host=localhost;dbname=COMP1006_Summer2022';
+        $username = 'root';
+        $password = 'root';
+        $db = new PDO($dsn, $username, $password);
+
+        //prepare the query 
+        $sql = "INSERT INTO students (name, email, age, location) VALUES (:name, :email, :age, :location)";
+
+        $statement = $db->prepare($sql);
+
+        //bindParam 
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':email', $email);
+        $statement->bindParam(':age', $age);
+        $statement->bindParam(':location', $location);
+
+        //execute 
+        $statement->execute();
+
+        //close cursor 
+        $statement->closeCursor();
+    }
+    ?>
+</body>
+
+</html>
