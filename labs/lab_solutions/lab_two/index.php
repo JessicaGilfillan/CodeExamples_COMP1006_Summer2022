86953a7c7f8e42a1eb03bfc962981385f6412ec8
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-06-15 12:42:24 -0400

    add client-side/server-side validation

--- labs/lab_solutions/lab_two/index.php
+++ labs/lab_solutions/lab_two/index.php
@@ -13,59 +13,81 @@
     <div class="container">
         <h1>Lab Two - Form Validation </h1>
         <!--add client-side form validation using HTML5 validation feature-->
+        <!--added required attribue, min and max for age and type="number" and type="email" for age and email-->
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <div class="form-group">
                 <label for="name"> Name: </label>
-                <input type="text" name="name" id="firstname" class="form-control">
+                <input type="text" name="name" id="firstname" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="email"> Email: </label>
-                <input type="text" name="email" id="email" class="form-control">
+                <input type="email" name="email" id="email" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="age"> Age: </label>
-                <input type="text" name="age" id="age" class="form-control">
+                <input type="number" name="age" id="age" class="form-control" min="17" max="110" required>
             </div>
             <div class="form-group">
                 <label for="location"> Location: </label>
-                <input type="text" name="location" id="location" class="form-control">
+                <input type="text" name="location" id="location" class="form-control" required>
             </div>
-                <input type="submit" value="Add Student" class="btn btn-primary" name="submit">
+            <input type="submit" value="Add Student" class="btn btn-primary" name="submit">
 
         </form>
     </div>
     <?php
     if (isset($_POST['submit'])) {
         $name = filter_input(INPUT_POST, 'name');
-        $email = filter_input(INPUT_POST, 'email');
-        $age = filter_input(INPUT_POST, 'age');
+        //add validation filter for email
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        //add validation filter for age
+        $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
         $location = filter_input(INPUT_POST, 'location');
 
         //add server-side form validation here!
+        //check if the user has provided required info 
+        if (!empty($name) || !empty($email) || !empty($age) || !empty($location)) {
+            //check that age and email are in the proper format 
+            if ($age !== false && $email !== false) {
+                //check that age is between 17 and 110 
+                if ($age >= 17 && $age <= 110) {
 
-        //connect to db 
-        $dsn = 'mysql:host=localhost;dbname=COMP1006_Summer2022';
-        $username = 'root';
-        $password = 'root';
-        $db = new PDO($dsn, $username, $password);
+                    //connect to db 
+                    $dsn = 'mysql:host=localhost;dbname=COMP1006_Summer2022';
+                    $username = 'root';
+                    $password = 'root';
+                    $db = new PDO($dsn, $username, $password);
 
-        //prepare the query 
-        $sql = "INSERT INTO students (name, email, age, location) VALUES (:name, :email, :age, :location)";
+                    //prepare the query 
+                    $sql = "INSERT INTO students (name, email, age, location) VALUES (:name, :email, :age, :location)";
 
-        $statement = $db->prepare($sql);
+                    $statement = $db->prepare($sql);
 
-        //bindParam 
-        $statement->bindParam(':name', $name);
-        $statement->bindParam(':email', $email);
-        $statement->bindParam(':age', $age);
-        $statement->bindParam(':location', $location);
+                    //bindParam 
+                    $statement->bindParam(':name', $name);
+                    $statement->bindParam(':email', $email);
+                    $statement->bindParam(':age', $age);
+                    $statement->bindParam(':location', $location);
 
-        //execute 
-        $statement->execute();
+                    //execute 
+                    $statement->execute();
 
-        //close cursor 
-        $statement->closeCursor();
+                    //close cursor 
+                    $statement->closeCursor();
+
+                    echo "<p> Success! </p>";
+                } else {
+                    echo "<p> You must enter an age between 17 and 110!</p>";
+                }
+            } else {
+                echo "<p> Please enter a valid email address and a number for your age. </p>";
+            }
+        } else {
+            echo "<p> Please provide your name, age, email and location! </p>";
+        }
     }
+
+
     ?>
 </body>
 
