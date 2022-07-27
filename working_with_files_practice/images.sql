f30b4871ee7ef9330a9fbf1e2902a96308945f73
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2022-07-27 09:04:35 -0400

    Add files via upload

---
+++ working_with_files_practice/images.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `images` (
+  `image_id` int NOT NULL,
+  `images_name` varchar(100) NOT NULL
+); 
---
+++ working_with_files_practice/process.php
@@ -0,0 +1,18 @@
+<?php 
+
+    //grab the information from the form 
+
+
+    //validate that the image is the right type and size and no errors on upload 
+
+
+    //move the file from the temp directory to where you would like it to go (i.e. images folder)
+
+
+    //store the information for the image in the pictures table 
+
+
+
+    //display the image on the page 
+
+    ?>
